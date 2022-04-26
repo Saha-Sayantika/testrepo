3825f74917a76cafb8de1f88f8f457769bf9e78a
Saha-Sayantika <104438245+Saha-Sayantika@users.noreply.github.com>
2022-04-26 19:03:01 +0600

    Add files via upload

---
+++ 1st class.py
@@ -0,0 +1,21 @@
+#python basics
+import random
+a = 7
+b = "sayantika"
+print(b[2:5])
+
+print(len(b))
+print(type(b))
+
+print(b[-5:])
+
+x=y=z=5
+print(x,y,z)
+
+x,y,z=6,5,4
+print(x,y,z)
+
+p=[1,2,3]
+print(max(p))
+
+print(random.randrange(0,4))
